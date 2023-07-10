9485547e5e7eb4c3b04f9b783b28bd9c89ea9545
Lee Soojin <76468868+Lee-Soojin@users.noreply.github.com>
2023-07-10 23:50:20 +0900

    feat: Create initial Signin page

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -1,5 +1,10 @@
-const SignIn = () => {
-  return <></>;
+"use client";
+
+import React from "react";
+import { SigninLayout } from "../../styles/auth";
+
+const SignIn: React.FC = () => {
+  return <SigninLayout>Sign in page</SigninLayout>;
 };
 
 export default SignIn;
