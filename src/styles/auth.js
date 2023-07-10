309159a19df8374887e94ceca513a2f835140316
Lee Soojin <76468868+Lee-Soojin@users.noreply.github.com>
2023-07-10 23:50:32 +0900

    design: Add SigninLayout

---
+++ src/styles/auth.js
@@ -0,0 +1,10 @@
+import { styled } from "styled-components";
+
+export const SigninLayout = styled.div`
+  display: flex;
+  width: 100%;
+  height: 100%;
+  min-width: 100vw;
+  min-height: 100vh;
+  background-color: #fff;
+`;
