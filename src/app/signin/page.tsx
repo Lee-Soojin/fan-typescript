8f7217750dcd7230e834c1e0616b3c7a5ef58460
Lee Soojin <76468868+Lee-Soojin@users.noreply.github.com>
2023-07-17 00:02:03 +0900

    feat: Add SignInForm component

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -2,9 +2,15 @@
 
 import React from "react";
 import { SigninLayout } from "../../styles/auth";
+import SignInForm from "./signin_form";
 
 const SignIn: React.FC = () => {
-  return <SigninLayout>Sign in page</SigninLayout>;
+  return (
+    <SigninLayout>
+      Sign in page
+      <SignInForm />
+    </SigninLayout>
+  );
 };
 
 export default SignIn;
